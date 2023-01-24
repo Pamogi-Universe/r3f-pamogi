e21ece32cee2d5332c7504665c1bb1eccaad4682
Bora Baflı <borabafli@gmail.com>
2023-01-25 00:48:27 +0300

    Left and top menus carried into UI folder

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import classes from "./Dashboard.module.css";
-import Tasks from "./dashboardMenu/tasks/Tasks";
-import ProjectDashboard from "./dashboardMenu/projectDashboard/ProjectDashboard";
-import Projects from "./dashboardMenu/projects/Projects";
-import Contributions from "./dashboardMenu/contributions/Contributions";
-import Investments from "./dashboardMenu/investments/Investments";
+import LeftMenuBar from "./UI/LeftMenuNav";
+import ProjectSelectionNav from "./UI/ProjectSelectionNav";
+import Tasks from "./menuContent/tasks/Tasks";
+import ProjectDashboard from "./menuContent/projectDashboard/ProjectDashboard";
+import Projects from "./menuContent/projects/Projects";
+import Contributions from "./menuContent/contributions/Contributions";
+import Investments from "./menuContent/investments/Investments";
 
 const Dashboard = (props) => {
   const [placeholderProjects, setPlaceholderProjects] = useState([
@@ -14,8 +16,8 @@ const Dashboard = (props) => {
     "Ipsum Project",
   ]);
   const [SelectedProject, setSelectedProject] = useState(placeholderProjects[0]);
-
   const [currentMenu, setCurrentMenu] = useState("Project Dashboard");
+
   const handleProjectSelection = (event) => {
     setSelectedProject(event.target.value);
   }
@@ -25,38 +27,9 @@ const Dashboard = (props) => {
   
   return (
     <div className={classes.dash_container}>
-      <div className={classes["dash_sidebar-container"]}>
-        <div className={classes["dash_sidebar-container_logo"]}>
-          <img src="/pamogi-logo.png" width={100} />
-        </div>
-        <div className={classes.dash_buttons_section}>
-          <div className={classes["dash_buttons-container"]}>
-            <button onClick={handleMenuButton} >Project Dashboard</button>
-            <button onClick={handleMenuButton}>Projects</button>
-            <button onClick={handleMenuButton}>Tasks</button>
-            <button onClick={handleMenuButton}>Contributions</button>
-            <button onClick={handleMenuButton}>Investments</button>
-          </div>
-        </div>
-      </div>
+      <LeftMenuBar handleMenuButton={handleMenuButton}  />
       <div className={classes["dash_main-canva"]}>
-        <div className={classes["dash_project-selection_container"]}>
-          <select className={classes["dash_project-selection_select"]} onChange={handleProjectSelection} >
-            {placeholderProjects.map((project, index) => {
-              return (
-                <option key={index} value={project}>
-                  {project}
-                </option>
-              );
-            })}
-          </select>
-          <div>
-            <button className={classes["dash_project-selection_create-button"]}>
-              {" "}
-              Create new project
-            </button>
-          </div>
-        </div>
+      <ProjectSelectionNav placeholderProjects={placeholderProjects} handleProjectSelection={handleProjectSelection} />     
         <button onClick={() => setDashboardVisible(false)}>Switch to 3D</button>
         {currentMenu === "Project Dashboard" && <ProjectDashboard projectData={SelectedProject} />}
         {currentMenu === "Projects" && <Projects projectData={SelectedProject} />}
